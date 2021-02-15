ac2119a83eb61d0247ef55d79babf6ebf36fc5d2
Tobias Klöpper <tobiaskloepper1@web.de>
2021-02-15 20:16:45 +0100

    added the airport model and the seed population

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airports = ["NYC", "SFO", "SEA", "JFK", "ABC", "SFS"]
+
+airports.each do |port|
+  Airport.create(code: port)
+end
