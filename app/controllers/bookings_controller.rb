7d043d3ce1879143d31e6ae87661ead4e5e835d3
Tobias Klöpper <tobiaskloepper1@web.de>
2021-03-06 11:55:42 +0100

    added a hidden input field for the flight_id to be able to save the booking object

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,13 +3,17 @@ class BookingsController < ApplicationController
     @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
     2.times { @booking.passengers.build }
-
-    #if @booking.save
-     # redirect_to bookings_path
-    #end
   end
 
   def create
-    @booking
+    @booking = Booking.create(booking_params)
+
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :commit, passengers_attributes: [:id, :name, :email])
   end
 end
