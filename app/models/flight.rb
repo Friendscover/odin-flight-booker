89c5946ccd5c54a5ade8937eaaae84eb32418332
Tobias Klöpper <tobiaskloepper1@web.de>
2021-03-07 12:15:19 +0100

    removed some debug code

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,10 +3,6 @@ class Flight < ApplicationRecord
   belongs_to :finish_airport, class_name: "Airport"
   has_many :bookings
 
-  def start_time_formatted
-    start.strftime("%d/%m/%Y")
-  end
-
   def display_flight_info
     "Starts at: #{start.strftime('%d/%m/%Y-%H:%M')} at #{start_airport.code} and arrives at #{finish_airport.code}"
   end
